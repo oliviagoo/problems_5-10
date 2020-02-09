2de9ac20df3f1cccefcc788654693e4908c3e180
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-10 12:52:17 +1300

    Update problem 5.py
    commenting

--- problem 5.py
+++ problem 5.py
@@ -1,10 +1,15 @@
 #problem 5
 #olivia g 10/2/20
+#importing the math program to use pi
 import math
+#setting the PI constant
 PI = math.pi
+#input - radius and rotations
 rad = float(input("Radius of the wheel in cm: "))
 rot = float(input("Number of rotations: "))
+#process - calculations
 diam = rad * 2
 circ = PI * diam
 dist = circ * rot
+#output - the dist. travelled
 print("Distance travelled: {:.2f}cm".format(dist))
