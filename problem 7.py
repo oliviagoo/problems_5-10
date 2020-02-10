71cbf50c65006d747cec66923f8ea3d7f9af0095
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-10 13:00:45 +1300

    Update problem 7.py
    commenting

--- problem 7.py
+++ problem 7.py
@@ -1,21 +1,21 @@
 #problem 7
 #olivia g 10/2/20
-
+#setting the constants - track prices
 SHORT = 1.79
 MEDIUM = 2.5
 LONG = 4.5
-
+#input - balance, and how many tracks were bought
 bal = float(input("What is your initial balance? "))
 purch_s = int(input("How many short tracks did you buy? "))
 purch_m = int(input("How many medium tracks did you buy? "))
 purch_l = int(input("How many long tracks did you buy? "))
-
+#process - calculations
 s_cost = purch_s * SHORT
 m_cost = purch_m * MEDIUM
 l_cost = purch_l * LONG
 tot_cost = s_cost + m_cost + l_cost
 newbal = bal - tot_cost
-
+#output - balance updates and costs
 print("------------------")
 print("Initial balance: ${:.2f}".format(bal))
 print("Purchased {} short tracks at ${:.2f}".format(purch_s, SHORT))
