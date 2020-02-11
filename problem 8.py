d7f3bc61aab1c4fb8153beb169e218e69ffff9a1
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air.local>
2020-02-11 14:28:47 +1300

    Update problem 8.py
    finishing the problem and commenting

--- problem 8.py
+++ problem 8.py
@@ -1,18 +1,18 @@
 #problem 8
 #olivia g 10/2/20
-
+#setting the constants - exchange rates
 USD = 1.154
 AUD = 1.22
-
+#input- balances in the accounts
 usbal = float(input("What is the balance in the US account? "))
 aubal = float(input("What is the balance in the AUS account? "))
-
+#process - calculations (converting to nzd)
 nz_usbal = usbal * USD
 nz_aubal = aubal * AUD
 nz_bal = nz_usbal + nz_aubal
-
+#output - printing the nzd values and final balance
 print("--------------------")
 print("US${:.2f} exchanges to NZ${:.2f}".format(usbal, nz_usbal))
 print("AUS${:.2f} exchanges to NZ${:.2f}".format(aubal, nz_aubal))
 print()
-#print("NZ balance: ${:.2f}")
+print("NZ balance: ${:.2f}".format(nz_bal))
